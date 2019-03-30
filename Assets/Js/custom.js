06efdabb56269f9e7d3f4bcc0a36edc2969108b6
Kogul Karna <cb.en.u4cse15325@cb.students.amrita.edu>
2019-03-30 11:19:19 +0530

    Pre-final Draft

---
+++ Assets/Js/custom.js
@@ -0,0 +1,19 @@
+$(document).ready(setHeights);
+$(document).ready(rightMost);
+$(window).resize(setHeights);
+$(window).resize(rightMost);
+
+function setHeights(){
+	var pinkInner = $(".pink-border");
+	var pinkOuter = $(".pink-border-outer");
+	for (var i = pinkInner.length - 1; i >= 0; i--) {
+		$(pinkOuter[i]).height(($(pinkInner[i]).height()+50));
+		$(pinkOuter[i]).width(($(pinkInner[i]).width()+50));
+	}
+}
+function rightMost(){
+	var bubblePosBox = $("#right-most");
+	var bubble = $("#bubble");
+	console.log(bubblePosBox);
+	bubble.position().right = bubblePosBox.position();
+}
